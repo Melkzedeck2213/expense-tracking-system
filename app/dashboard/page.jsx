024fc1576c92fd2f26c9337cd28060cc3592bc95
Meshack2213 <meshackzkl@gmail.com>
2025-06-25 12:47:22 +0300

    modified:   app/dashboard/page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,39 @@
 "use client";
 import NavBar from "@/components/NavBar";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { supabase } from "@/utils/supabaseClient";
 
 export default function Dashboard() {
+
+  const router = useRouter()
+
+  const [errorMessage, setErrorMessage] = useState("")
+  const [userEmail, setUserEmail] = useState("")
+  const [userName, setUserName] = useState("")
+
+useEffect(() => {
+  const protectPage = async () => {
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+      setErrorMessage(error.message);
+      router.push('/auth'); // stop further execution if there's an error
+    }
+
+    if (!data?.user) {
+      router.push('/auth');
+    } else {
+      setUserEmail(data.user.email);
+      setUsername(data.user.user_metadata?.username)
+    }
+  };
+
+  protectPage();
+}, [router]);
+
+
 
     const greetings = () => {
         const hour = new Date().getHours();
@@ -23,7 +55,7 @@ export default function Dashboard() {
       {/*Greeting*/}
 
       <div className="flex flex-col items-center justify-center h-screen">
-        <p>{greetings()},</p>
+        <p>{greetings()} {userName},you are logged in with the email {userEmail}</p>
         <p className="text-lg text-gray-700">Welcome to your dashboard!</p>
         <div className="mt-8">
           <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
